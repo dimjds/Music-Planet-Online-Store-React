9782855efd99bb00adb5e407ef0747a083d0c843
dimjds <127053521+dimjds@users.noreply.github.com>
2024-05-24 14:35:04 +0300

    some minor changes in front part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Card from './components/Card/Card.jsx';
+import Header from './components/Header';
+import Drawer from './components/Drawer.jsx';
+import Order from './components/Order.jsx';
+import Footer from './components/Footer.jsx';
+
+function App() {
+    const [items, setItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
+    const [searchValue, setSearch] = useState('');
+    const [cartOpened, setCartOpened] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        axios.get('http://localhost/reactApp/addItemScript.php?action=getItems')
+            .then(res => {
+                setItems(res.data);
+            })
+            .catch(error => {
+                console.error('Ошибка получения данных о товарах:', error);
+            });
+
+        const storedUsername = localStorage.getItem("username");
+        if (storedUsername) {
+            setUsername(storedUsername);
+            axios.get('http://localhost/reactApp/viewCartScript.php?action=getCartItems', {
+                params: {
+                    username: storedUsername
+                }
+            })
+                .then(res => {
+                    setCartItems(res.data);
+                    updateTotalPrice(res.data);
+                })
+                .catch(error => {
+                    console.error('Ошибка получения данных:', error);
+                });
+        }
+    }, []);
+
+    const updateTotalPrice = (cartItems) => {
+        const total = cartItems.reduce((acc, curr) => acc + parseFloat(curr.price), 0);
+        setTotalPrice(total.toFixed(2));
+    };
+
+    const onAddToCart = (obj) => {
+        if (!username) {
+            window.alert('Пожалуйста, сначала войдите в аккаунт, чтобы добавить товар в корзину.');
+            return;
+        }
+        
+        axios.post('http://localhost/reactApp/addToCartScript.php', { ...obj, username })
+            .then(response => {
+                window.location.reload(true);
+    
+                if (response.data.success) {
+                    setCartItems(prev => [...prev, obj]);
+                    updateTotalPrice([...cartItems, obj]);
+                    console.log(setCartItems);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка при добавлении товара. Попробуйте ещё раз:', error);
+                console.log(setCartItems);
+            });
+    };
+
+    const removeItem = (id) => {
+        axios.post('http://localhost/reactApp/removeItemScript.php', { id, username })
+            .then(response => {
+
+                window.location.reload(true);
+
+                if (response.data.success) {
+                    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+                    setCartItems(updatedCartItems);
+                    updateTotalPrice(updatedCartItems);
+                }
+            })
+            .catch(error => {
+                console.error("Ошибка при удалении товара. Попробуйте ещё раз:", error);
+            });
+    };
+
+    const searchingInput = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const filteredCartItems = cartItems.filter(item => item.username === username);
+
+    return (
+        <div className="wrapper clear">
+            {cartOpened &&
+                <Drawer
+                    onRemove={removeItem}
+                    items={filteredCartItems}
+                    onClose={() => setCartOpened(false)}
+                    total={totalPrice}
+                    productName={filteredCartItems[0]?.title}
+                />
+            }
+
+            <Header onClickCart={() => setCartOpened(true)} totalPrice={totalPrice} username={username} />
+            <div className="content p-10">
+                <div className="">
+                    <h1 className='font-bold text-3xl mb-10'>{searchValue ? `Мы, что-то нашли 👀 по запросу: "${searchValue}"` : 'Все товары 🎸'}</h1>
+                    <div className="search-block flex p-2 outline-none">
+                        <img className='ml-2' src="/img/search.svg" alt="Search" />
+                        <input onChange={searchingInput} value={searchValue} className='ml-2 outline-none' placeholder="Поиск..." />
+                        {searchValue && <img className='cursor-pointer' src="/img/btn-remove.svg" alt="remove" onClick={() => setSearch('')} />}
+                    </div>
+                </div>
+
+                <div className="flex flex-wrap gap-y-4 gap-x-5">
+                    {items.filter(obj => obj.title.toLowerCase().includes(searchValue.toLowerCase())).map((obj, index) => (
+                        <Card
+                            key={obj.id}
+                            title={obj.title}
+                            price={obj.price}
+                            imageUrl={obj.imageUrl}
+                            PlusClick={(obj) => onAddToCart(obj)}
+                        />
+                    ))}
+                </div>
+            </div>
+        <Footer/>
+            
+                    
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Header from './Header';
+import HeaderEmpty from './HeeaderEmpty';
+import Footer from './Footer';
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost/reactApp/contactScript.php',
+        {
+          name,
+          lastName,
+          email,
+          message
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+
+      if (response.data.success) {
+        alert('Сообщение успешно отправлено!');
+        setName('');
+        setLastName('');
+        setEmail('');
+        setMessage('');
+      } else {
+        alert('Ошибка отправки сообщения.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <HeaderEmpty/>
+      <form className='login' onSubmit={handleSubmit}>
+        <div className="flex justify-between">
+          <h2 className="text-3xl font-bold">Вход</h2>
+          <div className='removeBtn'>
+            <Link to="/home">
+              <img className="mb-6" src="/img/btn-remove.svg" alt="remove" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="text" className="form-control" id="name" placeholder='Введите имя' value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <input type="text" className="form-control" id="lastName" placeholder='Введите фамилию' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <input type="email" className="form-control" id="email" placeholder='Введите ваш Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <textarea className="form-control" id="message" rows="5" placeholder='Введите ваше сообщение' value={message} onChange={(e) => setMessage(e.target.value)} />
+        </div>
+
+        <button type="submit" className="reg-b-1">Отправить</button>
+      </form>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Contact;
